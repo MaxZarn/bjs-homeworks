6f3701a077736b571e9de2dfb42ca570fa2ff6a6
MaxZarn <m.zarn@mail.ru>
2019-03-24 15:58:12 +0300

    first commit

---
+++ variables/main.js
@@ -0,0 +1,24 @@
+// Задание 1:
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+console.log(result);
+
+// Задание 2:
+
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+let averageRating = (algebra + geography + physics) / 3;
+
+console.log(averageRating);
+
+// Задание 3:
+ 
+let name = 'Maxim';
+let message = `Привет, мир! Меня зовут ${name}.`
+
+console.log(message);
