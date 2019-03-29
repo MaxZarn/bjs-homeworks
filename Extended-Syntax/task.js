a56d0e65f06bc23911d60771a0993be5d791c1d9
MaxZarn <m.zarn@mail.ru>
2019-03-29 23:42:39 +0300

    complete

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -1,4 +1,4 @@
-
+"use strict"
 
 function calculateQuadraticEquation(){
     let a = +window.a.value;
@@ -12,7 +12,19 @@ function calculateQuadraticEquation(){
 
 function getResult(a,b,c){
     // код для задачи №1 писать здесь
-    //return x;
+    let x = [];
+    let d = b**2 - 4 * a * c;
+    if (d < 0) {
+        x = `Корней нет`;
+    } else if (d === 0) {
+        x[0] = -1 * b / 2 * a;
+    } else if (d > 0) {
+        x[0] = (-1 * b + d**(1/2)) / 2 * a;
+        x[1] = (-1 * b - d**(1/2)) / 2 * a;
+    }
+
+    console.log(x);
+    return x;
 }
 
 function calculateDrinkTask(){
@@ -24,17 +36,38 @@ function calculateDrinkTask(){
 
 function askDrink(name,dateOfBirthday){
     // код для задачи №2 писать здесь
-    //console.log(result)
-    //return result;
+    let now = new Date();
+     let age = now.getFullYear() - dateOfBirthday.getFullYear();
+     let result = null;
+     if (age >= 18) {
+        result = `Не желаете ли олд-фэшн, ${name}?`;
+     } else {
+        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+     }
+    console.log(result);
+    return result;
 }
 
 function calculateAverageRating(){
-    let marks = window.marks.value.split("").map(Number);
+    let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
     let averageMark = getAverageMark(marks);
     window.averageMark.textContent = averageMark;
 }
 
-function getAverageMark(marks){
+function getAverageMark(marks) {
     // код для задачи №3 писать здесь
-    //return averageMark;
+    let sumMarks = 0; // сумма оценок
+    let averageMark; // средняя оценка
+    // цикл для суммирования оценок:
+    for (let i = 0; i < marks.length; i++) {
+       sumMarks = sumMarks + marks[i];
+    if (marks.length > 5) {
+        console.log(`Количество оценок больше пяти`);
+        marks.splice(5);
+        }
+    }
+
+    averageMark = sumMarks / marks.length;
+
+    return averageMark;
 }
