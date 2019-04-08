0a217d06db6e41778a52ae8611b5356e9de92171
MaxZarn <m.zarn@mail.ru>
2019-04-08 22:59:49 +0300

    first commit

--- object-prototype/main.js
+++ object-prototype/main.js
@@ -1,3 +1,5 @@
+
+
 function initCheckBirthday() {
     const birthday = document.getElementById('birthday').value;
 
@@ -8,11 +10,20 @@ function initCheckBirthday() {
 
 function checkBirthday(birthday) {
     // код для задачи №1 писать здесь
+    let now = Date.now(); // текущая дата в миллисекундах
+    let date = new Date(birthday);
+    let diff = (now) - (+date); // разница
+    let age = diff / 31536000000; //возраст в годах 
+    if (age >= 18) {
+        return true;
+    } else if (age < 18) {
+        return false;
+    }
 }
 
 function initPrintAnimalSound() {
     const animal = {
-        sound: 'grrrr',
+        sound: 'grrrr', 
     };
 
     const result = getAnimalSound(animal);
@@ -22,6 +33,12 @@ function initPrintAnimalSound() {
 
 function getAnimalSound(animal) {
     // код для задачи №2 писать здесь
+    this.sound = animal.sound;
+    if (animal === undefined) {
+        return null;
+    } else if (animal !== undefined) {
+        return sound;
+    }
 }
 
 function initCalculateStatement() {
@@ -36,4 +53,20 @@ function initCalculateStatement() {
 
 function getAverageMark(marks) {
     // код для задачи №3 писать здесь
+    let roundedAverage;
+    for (let prop in marks) {
+        average = marks[prop];
+        average = averageScore(average);
+        //roundedAverage = Math.round(average);
+    }
+    roundedAverage = Math.round(average);
+    return roundedAverage;
 }
+
+function averageScore(x) {
+    let total = 0; // сумма оценок по предмету
+    for (let i = 0; x.length > i; i++) { //цикл для заполнения переменной c суммой оценок по предмету 
+      total += x[i];
+    }
+    return total / x.length; // возвращает среднее значение массива
+  }
